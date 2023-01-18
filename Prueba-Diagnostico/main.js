c2b1a224220e296214a54e5d5176d94541a24c58
Heylin <heylinloaiza.s@gmail.com>
2023-01-17 21:57:09 -1200

    add diagnostic text

---
+++ Prueba-Diagnostico/main.js
@@ -0,0 +1,114 @@
+// Prueba Diagnostico
+
+// I Parte: Teoría
+
+// 1. Respuesta pregunta 1
+
+// Colocar la respuesta acá
+// Los tipos de datos son: boolean, string, number y undefined.
+// Ejemplos:
+// boolean: true
+// String: "texto2"
+// Number: 48
+// undefined: let variable ;     
+//      console.log(undefined);
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+// con punto
+// objeto.miPropiedad = valorDeLaPropiedad
+
+// con corchetes
+// objeto[miPropiedad] = valorDeLaPropiedad
+
+// ====================================================================================================
+
+
+// 3. Respuesta pregunta 3
+
+// el += significa que la variable se suma asi misma más el valor que le indique
+// ej: variable = variable + 5 == variable += 5
+// 
+// let list = 1231;
+// list += 3234
+// console.log(list); // 4465
+// 
+// list += 'textDePrueba';
+// console.log(list); // '4465textDePrueba'
+
+// ====================================================================================================
+
+
+// 4. Respuesta pregunta 4
+
+// != verifica si el valor es diferente del otro valor y devuelve un valor booleano
+// !== es más estricto y compara si son del mismo tipo de dato para diferenciarlos
+// 
+// A. ambos resultados devuelven true porque em ambas comparaciones dice que si false es diferente de true, lo cual es cierto
+// B. != el resultado es false porque ambos son 10, sin importar su tipo
+//    !== el resultado es true porque compara si 10 en numero es diferente de '10' que es un string, son de diferemte tipo asi que la diferencia es verdadera
+// ====================================================================================================
+
+
+// 5. Respuesta pregunta 5
+
+// Una función anónima es aquella que no tiene nombre porque se declara dentro de una variable.
+
+// ====================================================================================================
+
+
+// 6. Respuesta pregunta 6
+
+// function declaration: tienen un nombre y sirve de manera global, es decir puedo llamar a la función
+//
+// function expression: esta función se declara dentro de una variable y es utilizada en momentos específicos
+
+// ====================================================================================================
+
+
+// 7. Respuesta pregunta 7
+
+// El método es una funcion que se encuentra dentro de un objeto
+
+// ====================================================================================================
+
+
+// 8. Respuesta pregunta 8
+
+// Recursividad: es cuando la función se llama dentro de sí misma una y otra vez hasta cumplir la condición
+
+// ====================================================================================================
+
+
+// II Parte: Ejercicios
+
+// 1. Respuesta pregunta 1
+
+// Colocar el código acá
+// let list = [1,6,8,2,4,8,7,9,3];
+// let par = [];
+// let impar = [];
+
+// function lista (){
+//   for (let i = 0; i < list.length; i++) {
+//     if (list[i] % 2 == 0) {
+//       par.push(list[i]);
+//     } else {
+//       impar.push(list[i]);
+//     }
+//   }
+//   console.log('lista par ' + par + ' y lista impar ' + impar);
+// }
+
+// lista(list, par, impar);
+
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+// Colocar el código acá
+
+// ====================================================================================================
